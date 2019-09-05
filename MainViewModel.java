afd48c454f93b4faa427dc0156cd3b671fdd95d9
mcameriere <31536331+mcameriere@users.noreply.github.com>
2019-09-05 06:37:41 +0200

    Create MainViewModel.java

---
+++ MainViewModel.java
@@ -0,0 +1,45 @@
+package com.hfad.tennis;
+
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    private MutableLiveData<String> score;
+    private TennisGame game;
+    private MutableLiveData<Boolean> canStartNewGame;
+
+    public MutableLiveData<String> getScore() {
+        if (score == null) {
+            score = new MutableLiveData<String>();
+            game = new TennisGame();
+            score.setValue(game.getDisplayedScore());
+        }
+        return score;
+    }
+
+    public MutableLiveData<Boolean> canStartNewGame() {
+        if (canStartNewGame == null) {
+            canStartNewGame = new MutableLiveData<Boolean>();
+            canStartNewGame.setValue(false);
+        }
+        return canStartNewGame;
+    }
+
+    public void playerOneScore() {
+        game.playerOneScores();
+        score.setValue(game.getDisplayedScore());
+        canStartNewGame.setValue(game.hasWinner());
+    }
+
+    public void playerTwoScore() {
+        game.playerTwoScores();
+        score.setValue(game.getDisplayedScore());
+        canStartNewGame.setValue(game.hasWinner());
+    }
+
+    public void newGame() {
+        game = new TennisGame();
+        score.setValue(game.getDisplayedScore());
+        canStartNewGame.setValue(game.hasWinner());
+    }
+}
